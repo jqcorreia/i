9e1e3846b1d7c7e0904247687ae5734ee1a09a8b
José Quadrado <jose.quadrado@movvo.com>
2017-11-08 19:05:59 +0000

    Video classification script improvements.

--- video_classification_tf.py
+++ video_classification_tf.py
@@ -6,8 +6,9 @@ from detector import DetectorMobilenetSSD
 import utils
 
 cap = cv2.VideoCapture(sys.argv[1])
-detector = DetectorMobilenetSSD("zoo/head11.pb")
+detector = DetectorMobilenetSSD("zoo/nas.pb")
 
+max_count = 0
 while True:
     running, image = cap.read()
 
@@ -16,11 +17,11 @@ while True:
 
     (boxes, scores, classes, num) = detector.detect(image)
     (h, w) = image.shape[:2]
+    count = 0
     for i in range(int(num[0])):
-        # print(boxes[0][i], scores[0][i], classes[0][i])
-        if scores[0][i] < 0.3:
+        if scores[0][i] < 0.5 or classes[0][i] != 1.0:
             continue
-
+        count +=1
         box = boxes[0][i] * np.array([h, w, h, w])
         (startY, startX, endY, endX) = box.astype("int")
         cv2.rectangle(image, (startX, startY), (endX, endY),
@@ -28,5 +29,8 @@ while True:
         y = startY - 15
         cv2.putText(image, str(scores[0][i]), (startX, y),
             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
+    if count > max_count:
+        print(count)
+        max_count = count
     cv2.imshow("Output", image)
     cv2.waitKey(1)
